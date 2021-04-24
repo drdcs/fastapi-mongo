ffbc26bd1ba27380db8f54bb90414c35679a6c8c
drdcs <diptihdl@gmail.com>
2021-04-24 15:54:51 +0530

    added routes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
 import uvicorn
+import sys
+import os
+
+# The below code is to find the path for the driver program.
+# This will remove module not found error.
+
+cwd = os.getcwd()
+sys.path.insert(0, cwd)
 
 if __name__ == '__main__':
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
